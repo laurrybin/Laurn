1e9d0e5c750a37ebcfbf27e2914082fa01a1c514
laurrybin <laurrybin@gmail.com>
2026-09-04 21:57:03 -0500

    chore(unreal-example): remove trailing whitespace

--- examples/unreal/Source/LaurnExample/ExampleCharacter.h
+++ examples/unreal/Source/LaurnExample/ExampleCharacter.h
@@ -24,7 +24,7 @@ class LAURNEXAMPLE_API AExampleCharacter : public ACharacter
 public:
     AExampleCharacter();
     virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-    
+
     void MoveForward(float Value);
     void MoveRight(float Value);
 };
